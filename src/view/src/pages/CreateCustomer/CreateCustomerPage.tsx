35ec2126f265214825b24fcfee06fd9351a0067f
Belal Ahmad <belalabomoalsh@gmail.com>
2025-05-23 00:34:22 +0300

    import font-faces

--- src/view/src/pages/CreateCustomer/CreateCustomerPage.tsx
+++ src/view/src/pages/CreateCustomer/CreateCustomerPage.tsx
@@ -1,9 +1,10 @@
 import TopOfPage from '../../shared/components/TopOfPage/TopOfPage';
 import CreateCustomerForm from './CreateCustomerForm';
+import styles from './CreateCustomerPage.module.scss';
 
 export default function CreateCustomerPage() {
   return (
-    <main>
+    <main className={styles.main}>
       <TopOfPage title='New Customer' showAddButton={false} />
 
       <CreateCustomerForm />
